13d5c09859f5c80133fd3172fa1046e478f3a0e5
Thales Soares Mendonça <thalesthalessoaresmendonca12@gmail.com>
2026-04-15 10:51:32 -0300

    Update ativ12.java

--- ativ12.java
+++ ativ12.java
@@ -4,14 +4,14 @@ import java.util.Scanner;
 
 public class ativ12 {
     public static void main(String[] args) {
-        Scanner goku = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int preco,num;
         System.out.println(("digite 1 para pagar a vista 10% desc"));
         System.out.println("digite 2 para pagar no cartão com desconto de 5%");
         System.out.println("digite 3 para pagar 2x preço normal");
-        preco= goku.nextInt();
+        preco= sc.nextInt();
         System.out.println("qual o valor do produto?");
-        num =goku.nextInt();
+        num =sc.nextInt();
         if (preco==1){
             System.out.println(num*0.9);}
         else if (preco==2){
